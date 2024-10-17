07d476273b86c9aa56468d084fe113f0f6d69b61
esimorre <esimorre@gmail.com>
2024-10-17 22:47:34 +0200

    num weeks

--- mycalendar.py
+++ mycalendar.py
@@ -2,9 +2,34 @@ from calendar import LocaleTextCalendar
 
 
 class MyCalendar(LocaleTextCalendar):
+    # year -> [num_week1, ...]
+    _nbweek_monthes = {}
+    def __init__(self, firstweekday, locale):
+        super().__init__(firstweekday, locale)
+
+    def _update_data(self, year : int):
+        if year not in self._nbweek_monthes:
+            num = 1
+            weeks_data = self.monthdays2calendar(year,1)
+            if weeks_data[0][0][1] in (1, 2, 3): num = 2
+            data = [num]
+            for m in range(1, 12):
+                weeks_data = self.monthdays2calendar(year, m + 1)
+                nbweeks = len(weeks_data)
+                num = num + nbweeks
+                if weeks_data[0][0][1] < m : num -= 1
+                data.append( num)
+            self._nbweek_monthes[year] = data
+
+    def monthdays2calendar_ext(self, year : int,  month : int):
+        """ extends monthdays2calendar with num weeks """
+        self._update_data(year)
+        w1 = self._nbweek_monthes[year][month-1]
+        md2c = self.monthdays2calendar(year, month)
+        return [(w1+ns, ls) for ns, ls in enumerate(md2c)]
 
     def days_week_labels(self, len : int = 2):
-        return [self.formatweekday(i, 2) for i in self.iterweekdays()]
+        return [self.formatweekday(i, len) for i in self.iterweekdays()]
 
     def month_name(self, year : int, month : int):
         return self.formatmonthname(year, month, 30, withyear=False)
--- test/test_calendar.py
+++ test/test_calendar.py
@@ -6,8 +6,11 @@ from mycalendar import MyCalendar
 
 class TestCalendar(unittest.TestCase):
 
+    def setUp(self):
+        self.cal = MyCalendar(firstweekday=0, locale="fr")
+
     def test_basic(self):
-        cal = MyCalendar(firstweekday=0, locale="fr")
+        cal = self.cal
         now = datetime.now()
         for q in cal.itermonthdays4(now.year, now.month):
             w, m, d, wd = q
@@ -15,3 +18,11 @@ class TestCalendar(unittest.TestCase):
         print(cal.days_week_labels())
         print(cal.formatmonthname(now.year, now.month, 30, withyear=False))
 
+    def test_monthdays2calendar(self):
+        print(self.cal.monthdays2calendar(2024, 1))
+        print(self.cal.monthdays2calendar(2024, 2))
+
+    def test_monthdays2calendar_ext(self):
+        print(self.cal.monthdays2calendar_ext(2024, 1))
+        print(self.cal.monthdays2calendar_ext(2024, 12))
+        print(self.cal.monthdays2calendar_ext(2025, 1))
