a7e1701b120db02f340646dab48f4b917aeabac9
esimorre <esimorre@gmail.com>
2024-10-28 00:20:35 +0100

    PanGesture

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 
 import flet as ft
 from controls import Month
@@ -6,14 +6,13 @@ from controls import Month
 
 class CalendarApp(ft.Column):
     nb_monthes = 3
-    def __init__(self):
+    def __init__(self, dtime : datetime):
         super().__init__()
-        now = datetime.now()
 
         self.width = 400
         self.controls = []
         for m in range(self.nb_monthes):
-            self.controls.append( Month(2024, now.month + m) )
+            self.controls.append( Month(2024, dtime.month + m) )
 
 class Drawer(ft.NavigationDrawer):
 
@@ -40,11 +39,39 @@ class Drawer(ft.NavigationDrawer):
         ],
         )
 
-def main(page: ft.Page):
+class CalendarCache:
+    def __init__(self):
+        self.cache = {}
 
+    def get(self, dtime : datetime):
+        key = "%d:%d" % (dtime.year, dtime.month)
+        if key not in self.cache:
+            self.cache[key] = CalendarApp(dtime)
+        return self.cache[key]
+
+class PanGesture(ft.GestureDetector):
+    def __init__(self, now : datetime):
+        super().__init__(
+        on_horizontal_drag_update=self.on_pan,
+        drag_interval=500,
+        )
+        self.current = now
+        self.cache = CalendarCache()
+        self.content = self.cache.get(now)
+
+    def on_pan(self, e: ft.DragUpdateEvent):
+        dmonth = timedelta(weeks=CalendarApp.nb_monthes*4)
+        if e.primary_delta < 0: dmonth *= -1
+        self.current += dmonth
+        print(e.primary_delta, self.current)
+        self.content = self.cache.get(self.current)
+        self.page.update()
+
+def main(page: ft.Page):
+    now = datetime.now()
     drawer = Drawer()
-    def on_pan(e: ft.DragUpdateEvent):
-        print(e)
+
+
     def on_sidebar(e : ft.ControlEvent):
         print("on_sidebar", e)
         page.open(drawer)
@@ -66,11 +93,7 @@ def main(page: ft.Page):
     page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
     page.scroll = ft.ScrollMode.ADAPTIVE
 
-    gd = ft.GestureDetector(
-        on_horizontal_drag_update=on_pan,
-        drag_interval=500,
-        content=CalendarApp()
-    )
+    gd = PanGesture(now)
 
     page.add(gd)
 
